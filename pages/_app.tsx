fd7cf030a27ddf99c0a6f2733db49eee43c092ef
SweetCuteWitch <deworewitch@gmail.com>
2025-09-14 16:08:33 +0300

    add pwa

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,25 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import Layout from '../components/Layout';
-import Head from 'next/head';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Layout from "../components/Layout";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Head>
         <title>Next-recipes</title>
+        <meta
+          name="description"
+          content="Приложение для управления рецептами"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Recipes" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-192x192.svg" />
+        <link rel="icon" type="image/svg+xml" href="/icon-192x192.svg" />
       </Head>
       <Component {...pageProps} />
     </Layout>
